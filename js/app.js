b1fa54bdea23d1ef84007ca523514c05b861e87e
jmcia2020 <jeanmmcmahon@gmail.com>
2021-01-05 20:37:25 -0600

    added eslintr git ignore and markdown

--- js/app.js
+++ js/app.js
@@ -1,5 +1,72 @@
 'use strict'
 console.log ('And Java is LIVE!!!')
 
+// //prompt our user for input data
+var userName = prompt('What is your name?');
+console.log(userName);
+
+Greet the user
+alert('Hello ' + userName);
+
+
+//Validate user input. 
+var toLowerCase = userName.toLowerCase();
+console.log(toLowerCase);
+
+//Validate the input using conditional logic using strict equals.
+
+// if(condition){do something}else{do something else}
+
+
+// if(toLowerCase === 'craig' || toLowerCase === 'bob')//FALSE
+
+// //WILL NOT RUN
+// {
+
+//   alert('Welcome back to my page, ' + userName);
+
+// } 
+
+
+// //DO THIS
+// else {
+
+//   alert('Welcome new user to my page.');
+
+// }
+
+
+// logical operator example ---------------
+// var myBooleenValueTrue = true;
+// var anotherBooleenValueFalse = false;
+// var thirdBooleenValueTrue = true;
+//else if
+// var myBooleenValueTrue = true;
+// var anotherBooleenValueFalse = false;
+// var thirdBooleenValueTrue = false;
+//next else if
+// var myBooleenValueTrue = false;
+// var anotherBooleenValueFalse = false;
+// var thirdBooleenValueTrue = true;
+//else
+// var myBooleenValueTrue = false;
+// var anotherBooleenValueFalse = false;
+// var thirdBooleenValueTrue = false;
+
+// if (myBooleenValueTrue && thirdBooleenValueTrue){
+//   alert('The && will run if both are true');
+//   console.log('both values were true');
+// } else if(myBooleenValueTrue || anotherBooleenValueFalse){
+//   alert('The || will run if both are true');
+//   console.log('one of values was true');
+// } else if(thirdBooleenValueTrue){
+//   alert('the 3rd value was tru so this will run');
+//   console.log('thirdBooleenValueTrue was true');
+// } else {
+//   alert('nope we reached the else statement.');
+//   console.log('There were no true values');
+// }
+
+
 
 
