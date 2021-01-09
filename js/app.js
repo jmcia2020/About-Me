73df8715e13c28d8695422cd0ae4c6139fa06f90
jmcia2020 <jeanmmcmahon@gmail.com>
2021-01-09 16:45:34 -0600

    Lab 2 questions complete changes to ol class and location

--- js/app.js
+++ js/app.js
@@ -1,76 +1,123 @@
-'use strict';
-console.log ('And Java is LIVE!!!')
+"use strict";
+console.log ('And Java is LIVE!!!');
 
 // //prompt our user for input data
 var userName = prompt('What is your name?');
 console.log(userName);
-
+// 
 // Greet the user
-alert('Hello ' + userName + '.' + 'Thanks for visitng my site');
+alert
+('Hello ' + userName + '. Thanks for visitng my site.');
+
+var siblings = 
+prompt('Do  you have siblings?').toLowerCase();
+console.log(siblings);
+
+if(siblings === 'y' || siblings === 'yes')
+{
+  alert ("Me too!");
+}
+else if(siblings === 'n' || siblings === 'no')
+{
+  alert("Being an only child has it's advantages.");
+}
+// 
+var travel = prompt('Have you ever been to Germany?').toLowerCase();
+console.log(travel);
+
+if(travel === 'y' || travel === 'yes')
+{
+  alert ("The people there are so nice!");
+}
+else
+{
+  alert ("There's always time for a new adventure!");
+}
+// 
+var catsDogs = prompt('Are you a cat or a dog person?').toLowerCase();
+console.log(catsDogs);
+
+if(catsDogs === 'dog' || catsDogs ==='dogs')
+{
+  alert ("Me too!");
+}
+else if(catsDogs === 'cat' || catsDogs === 'cats')
+{
+  alert("I never had a kitty cat.");
+}
+// 
+var sushi = prompt('Do you like sushi?').toLowerCase();
+console.log(sushi);
 
 
-//Validate user input. 
-var toLowerCase = userName.toLowerCase();
-console.log(toLowerCase);
+if(sushi === 'y' || sushi === 'yes')
+{
+  alert ("AAC rolls don't count!");
+}
+else
+{
+  alert("Adventure isn't for everyone when it comes to food.");
+}
 
-//Validate the input using conditional logic using strict equals.
+// 
 
-if(condition){do something}else{do something else}
+// //Validate user input. 
+// var toLowerCase = userName.toLowerCase();
+// console.log(toLowerCase);
 
-if(familyPlan === 'Y' || 'Yes'){do something}else{do something else}
-
-if(toLowerCase === 'craig' || toLowerCase === 'bob')//FALSE
-
-
-Q 6
-var userGuess - prompt('Pick )
-
-// //WILL NOT RUN
-// {
-
-//   alert('Welcome back to my page, ' + userName);
-
-// } 
+// //Validate the input using conditional logic using strict equals.
 
 
-// //DO THIS
-// else {
+// if(siblings === 'Y' || 'Yes'){do something}else{do something else}
 
-//   alert('Welcome new user to my page.');
-
-// }
+// if(toLowerCase === 'craig' || toLowerCase === 'bob')//FALSE
 
 
-// logical operator example ---------------
-// var myBooleenValueTrue = true;
-// var anotherBooleenValueFalse = false;
-// var thirdBooleenValueTrue = true;
-//else if
-// var myBooleenValueTrue = true;
-// var anotherBooleenValueFalse = false;
-// var thirdBooleenValueTrue = false;
-//next else if
-// var myBooleenValueTrue = false;
-// var anotherBooleenValueFalse = false;
-// var thirdBooleenValueTrue = true;
-//else
-// var myBooleenValueTrue = false;
-// var anotherBooleenValueFalse = false;
-// var thirdBooleenValueTrue = false;
+// Q 6
+// var userGuess - prompt('Pick )
 
-// if (myBooleenValueTrue && thirdBooleenValueTrue){
-//   alert('The && will run if both are true');
-//   console.log('both values were true');
-// } else if(myBooleenValueTrue || anotherBooleenValueFalse){
-//   alert('The || will run if both are true');
-//   console.log('one of values was true');
-// } else if(thirdBooleenValueTrue){
-//   alert('the 3rd value was tru so this will run');
-//   console.log('thirdBooleenValueTrue was true');
-// } else {
-//   alert('nope we reached the else statement.');
-//   console.log('There were no true values');
-// }
+// // //WILL NOT RUN
+// // {
+
+// //   alert('Welcome back to my page, ' + userName);
+
+// // } 
+
+
+// // //DO THIS
+// // else {
+
+
+// // logical operator example ---------------
+// // var myBooleenValueTrue = true;
+// // var anotherBooleenValueFalse = false;
+// // var thirdBooleenValueTrue = true;
+// //else if
+// // var myBooleenValueTrue = true;
+// // var anotherBooleenValueFalse = false;
+// // var thirdBooleenValueTrue = false;
+// //next else if
+// // var myBooleenValueTrue = false;
+// // var anotherBooleenValueFalse = false;
+// // var thirdBooleenValueTrue = true;
+// //else
+// // var myBooleenValueTrue = false;
+// // var anotherBooleenValueFalse = false;
+// // var thirdBooleenValueTrue = false;
+
+// // if (myBooleenValueTrue && thirdBooleenValueTrue){
+// //   alert('The && will run if both are true');
+// //   console.log('both values were true');
+// // } else if(myBooleenValueTrue || anotherBooleenValueFalse){
+// //   alert('The || will run if both are true');
+// //   console.log('one of values was true');
+// // } else if(thirdBooleenValueTrue){
+// //   alert('the 3rd value was tru so this will run');
+// //   console.log('thirdBooleenValueTrue was true');
+// // } else {
+// //   alert('nope we reached the else statement.');
+// //   console.log('There were no true values');
+// // }
 
 
 
